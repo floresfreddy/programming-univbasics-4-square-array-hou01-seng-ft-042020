e28d745e2812977968416e74edb89fc22fd8767f
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-04 04:18:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,8 @@ def square_array(array)
   counter = 0 
   new_array = []
   while array[counter] do 
-    new_array = array[counter]
+    new_array = (array[counter]*array[counter])
     counter += 1 
   end
+  new_array
 end
